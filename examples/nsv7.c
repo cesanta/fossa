175c59934c1043e23f5646747a5b16eca15393fb
Sergey Lyubka <valenok@gmail.com>
2014-06-15 22:14:10 +0100

    Javascript event handler getting called

--- examples/nsv7.c
+++ examples/nsv7.c
@@ -20,34 +20,61 @@
 #include "net_skeleton.h"
 #include "v7.h"
 
-#ifndef NSV7_SCRIPT
-#define NSV7_SCRIPT "nsv7.js"
-#endif
+static int s_received_signal = 0;
+
+static void elog(int do_exit, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  va_end(ap);
+  fputc('\n', stderr);
+  if (do_exit) exit(EXIT_FAILURE);
+}
+
+static void signal_handler(int sig_num) {
+  signal(sig_num, signal_handler);
+  s_received_signal = sig_num;
+}
 
 static void ev_handler(struct ns_connection *nc, enum ns_event ev, void *p) {
   struct v7 *v7 = (struct v7 *) nc->server->server_data;
+  
+  printf("C handler: %p %d %p\n", nc, ev, p);
 
-  // Push parameters then event handler on stack
+  // Call javascript event handler
+  v7_exec(v7, "ns.ev_handler");
   v7_push(v7, V7_NUM)->v.num = ev;
   v7_push(v7, V7_NUM)->v.num = (unsigned long) p;
-  v7_exec(v7, "ns.ev_handler");
-
-  // Call event handler
-  v7_call(v7, v7_top(v7) - 3);
+  v7_call(v7, v7_top(v7) - 3, 2);
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  const char *script = "nsv7.js";
   struct ns_server server;
   struct v7 *v7 = v7_create();
+  int i;
+  
+  // Parse command line options
+  for (i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+      script = argv[++i];
+    } else {
+      elog(1, "Usage: %s [-script FILE]", argv[0]);
+    }
+  }
+
+  signal(SIGTERM, signal_handler);
+  signal(SIGINT, signal_handler);
 
   v7_init_stdlib(v7);
-  v7_exec_file(v7, NSV7_SCRIPT);
+  v7_exec_file(v7, script);
 
   ns_server_init(&server, v7, ev_handler);
-  
-  for (;;) {
+  ns_bind(&server, "8080");
+  while (s_received_signal == 0) {
     ns_server_poll(&server, 1000);
   }
+  printf("Existing on signal %d\n", s_received_signal);
   ns_server_free(&server);
 
   return EXIT_SUCCESS;
--- examples/nsv7.js
+++ examples/nsv7.js
@@ -1,2 +1,7 @@
-a = 2 + 3;
-print('hello world ', 12 + 45 * a, ' ', ns, '\n');
+print('Initializing net skeleton scripting...', '\n');
+
+var ns =  {
+  ev_handler: function(a, b) {
+    print('a: ', a, ', b: ', b, '\n');
+  }
+};

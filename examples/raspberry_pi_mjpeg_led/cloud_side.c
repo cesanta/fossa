81190905547722bcdaa5b92012b9901552a0d03d
Marko Mikulicic <mkm@cesanta.com>
2014-11-03 21:22:42 +0000

    Massive simplification of the C part of the RPi example

--- examples/raspberry_pi_mjpeg_led/cloud_side.c
+++ examples/raspberry_pi_mjpeg_led/cloud_side.c
@@ -12,12 +12,7 @@
  */
 #include "fossa.h"
 
-#define HEADER_SIZE sizeof(uint32_t)
-
-static int s_received_signal = 0;
 static struct ns_serve_http_opts web_root_opts = { "./web_root" };
-
-static time_t last_frame_timestamp;
 
 /* Forwards the jpeg frame data to all open mjpeg connections.
  *
@@ -31,17 +26,6 @@ static time_t last_frame_timestamp;
  */
 static void push_frame_to_clients(struct ns_mgr *mgr,
                                   const struct websocket_message *wm) {
-  if (wm->size < HEADER_SIZE) {
-    printf("Got invalid websocket frame: too small");
-    return;
-  }
-
-  time_t ts = (time_t)ntohl(*(uint32_t*)wm->data);
-  time_t now = time(NULL);
-  printf("Received frame age: %lds; age of previous frame: %lds, lag: %lds\n",
-         now-ts, now-last_frame_timestamp, now-ts + now-last_frame_timestamp);
-  last_frame_timestamp = ts;
-
   struct ns_connection *nc;
   /** mjpeg connections are tagged with the NSF_USER_2 flag so we can find them
    * my scanning the connection list provided by the fossa manager. */
@@ -49,8 +33,8 @@ static void push_frame_to_clients(struct ns_mgr *mgr,
     if (!(nc->flags & NSF_USER_2)) continue;  // Ignore un-marked requests
 
     ns_printf(nc, "--w00t\r\nContent-Type: image/jpeg\r\n"
-              "Content-Length: %lu\r\n\r\n", (unsigned long) wm->size - HEADER_SIZE);
-    ns_send(nc, wm->data + HEADER_SIZE, wm->size - HEADER_SIZE);
+              "Content-Length: %lu\r\n\r\n", (unsigned long) wm->size);
+    ns_send(nc, wm->data, wm->size);
     ns_send(nc, "\r\n", 2);
     printf("Image pushed to %p\n", nc);
   }
@@ -64,6 +48,7 @@ static void send_command_to_the_device(struct ns_mgr *mgr,
   struct ns_connection *nc;
   for (nc = ns_next(mgr, NULL); nc != NULL; nc = ns_next(mgr, nc)) {
     if (!(nc->flags & NSF_IS_WEBSOCKET)) continue;  // Ignore non-websocket requests
+
     ns_send_websocket_frame(nc, WEBSOCKET_OP_TEXT, cmd->p, cmd->len);
     printf("Sent API command [%.*s] to %p\n", (int) cmd->len, cmd->p, nc);
   }
@@ -115,15 +100,6 @@ static void ev_handler(struct ns_connection *nc, int ev, void *ev_data) {
   }
 }
 
-/* Intercepts signals in order to gracefully terminate the connections.
- * We simply set a global flag so that our poll loop will be interrupted
- * the next time ns_poll returns.
- */
-static void signal_handler(int sig_num) {
-  signal(sig_num, signal_handler);
-  s_received_signal = sig_num;
-}
-
 int main(int argc, char *argv[]) {
   struct ns_mgr mgr;
 
@@ -131,10 +107,6 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Usage: %s <listening_addr>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
-
-  signal(SIGTERM, signal_handler);
-  signal(SIGINT, signal_handler);
-  signal(SIGPIPE, SIG_IGN);
 
   printf("Listening on: [%s]\n", argv[1]);
 
@@ -155,12 +127,7 @@ int main(int argc, char *argv[]) {
   /** We explicitly hand over control to the Fossa manager
    * in this event loop and we can easily multiplex other activities.
    */
-  while (s_received_signal == 0) {
+  for(;;) {
     ns_mgr_poll(&mgr, 1000);
   }
-  ns_mgr_free(&mgr);
-
-  printf("Quitting on signal %d\n", s_received_signal);
-
-  return EXIT_SUCCESS;
 }

eb5b2812940f88523568804ea8017184413e0df7
Sergey Lyubka <valenok@gmail.com>
2014-11-15 14:37:17 +0000

    Readability changes, added comment

--- examples/restful_client/restful_client.c
+++ examples/restful_client/restful_client.c
@@ -5,8 +5,11 @@
 
 #include "fossa.h"
 
+/* RESTful server host and request URI */
 static const char *s_target_address = "ajax.googleapis.com:80";
-static int s_exit = 0;
+static const char *s_request = "/ajax/services/search/web?v=1.0&q=cesanta";
+
+static int s_exit_flag = 0;
 
 static void ev_handler(struct ns_connection *nc, int ev, void *ev_data) {
   struct http_message *hm = (struct http_message *) ev_data;
@@ -18,18 +21,17 @@ static void ev_handler(struct ns_connection *nc, int ev, void *ev_data) {
       if (connect_status == 0) {
         printf("Connected to %s, sending request...\n", s_target_address);
         ns_printf(nc, "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n",
-                  "/ajax/services/search/web?v=1.0&q=cesanta",
-                  "ajax.googleapis.com");
+                  s_request, s_target_address);
       } else {
         printf("Error connecting to %s: %s\n",
                s_target_address, strerror(connect_status));
-        s_exit = 1;
+        s_exit_flag = 1;
       }
       break;
     case NS_HTTP_REPLY:
       printf("Got reply:\n%.*s\n", (int) hm->body.len, hm->body.p);
       nc->flags |= NSF_FINISHED_SENDING_DATA;
-      s_exit = 1;
+      s_exit_flag = 1;
       break;
     default:
       break;
@@ -45,7 +47,7 @@ int main(void) {
   ns_set_protocol_http_websocket(nc);
 
   printf("Starting RESTful client against %s\n", s_target_address);
-  while (s_exit == 0) {
+  while (s_exit_flag == 0) {
     ns_mgr_poll(&mgr, 1000);
   }
   ns_mgr_free(&mgr);

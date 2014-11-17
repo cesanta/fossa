7e51ad438f552e0454f583e75d43f76f403ef5fe
Marko Mikulicic <mkm@cesanta.com>
2014-11-17 11:01:53 +0100

    Added toc autoscrolling

--- docs/doc.js
+++ docs/doc.js
@@ -7,6 +7,32 @@ $(function() {
     if (item.length) { return item; }
   });
 
+  var lastTocScroll = 0;
+  var lastTocScrollHuman = true;
+
+  // Scroll an element so that it becomes visible
+  // unless the user recently scrolled the toc div.
+  // If the user scrolls to the top or the bottom of an overflow:scroll
+  // element, the browser will scroll the main area, which will cause
+  // another element to be activated (and scrolled to) in the toc pane.
+  function scrollToView(pane, element) {
+    if (new Date().getTime() - lastTocScroll < 2000) return;
+
+    var y = element.position().top;
+    if (y < 0 || y > pane.height()) {
+      lastTocScrollHuman = false;
+      pane.scrollTo(element);
+    }
+  }
+
+  topMenu.scroll(function() {
+    if (lastTocScrollHuman) {
+      lastTocScroll = new Date().getTime();
+    } else {
+      lastTocScrollHuman = true;
+    }
+  });
+
   $(window).scroll(function() {
     // Get container scroll position
    var fromTop = $(this).scrollTop() + 40;
@@ -19,8 +45,12 @@ $(function() {
    // Get the id of the current element
    cur = cur[cur.length-1];
    var id = cur && cur.length ? cur[0].id : "";
-   menuItems
-     .parent().removeClass(className)
-     .end().filter("[href=#"+id+"]").parent().addClass(className);
+   var item = menuItems.parent().removeClass(className)
+                       .end().filter("[href=#"+id+"]").parent();
+
+   if (item.length != 0) {
+     item.addClass(className);
+     scrollToView(topMenu, item);
+   }
  });
 });

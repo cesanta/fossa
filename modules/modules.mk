f8a5356a31523fc4cd6cc536f9c6678ab8c5fd64
Marko Mikulicic <mkm@cesanta.com>
2014-11-21 20:07:47 +0100

    Use `frozen` module bundled in the substree

--- modules/modules.mk
+++ modules/modules.mk
@@ -1,3 +1,3 @@
-FR = ../../frozen
+FR = ../deps/frozen
 HEADERS = common.h iobuf.h net.h $(FR)/frozen.h sha1.h util.h http.h json-rpc.h mqtt.h
 SOURCES = iobuf.c net.c $(FR)/frozen.c http.c sha1.c util.c json-rpc.c mqtt.c

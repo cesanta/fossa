3407b3cf8f5e20fc7fd5978e5e7f851a1b4a1cd9
Ake Hedman, Paradise of the Frog <akhe@grodansparadis.com>
2015-08-18 20:28:00 +0200

    Take away annoying warnings when _CRT_SECURE_NO_WARNINGS is already defined.

--- net_skeleton.h
+++ net_skeleton.h
@@ -25,7 +25,9 @@
 #undef _UNICODE                 // Use multibyte encoding on Windows
 #define _MBCS                   // Use multibyte encoding on Windows
 #define _INTEGRAL_MAX_BITS 64   // Enable _stati64() on Windows
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS // Disable deprecation warning in VS2005+
+#endif
 #undef WIN32_LEAN_AND_MEAN      // Let windows.h always include winsock2.h
 #define _XOPEN_SOURCE 600       // For flockfile() on Linux
 #define __STDC_FORMAT_MACROS    // <inttypes.h> wants this for C++
